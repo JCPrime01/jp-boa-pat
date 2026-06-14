224e3533f82ff9867ac0333a519be4fd657d7590
JCPrime01 <vanzokiller@gmail.com>
2026-06-14 12:14:36 -0300

    chore: shareCode Q348YI0C5Y7 -> R7PR6GIG9PU (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=Q348YI0C5Y7&afp_campaign=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/zy1z1vkz?shareCode=R7PR6GIG9PU&afp_campaign=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }

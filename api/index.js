372ed90d13dc85d19d41e71df288ec7e6d88dedc
JCPrime01 <vanzokiller@gmail.com>
2026-06-28 01:49:24 -0300

    chore: shareCode KBBILKJKNAB -> B8GOCJSYQK4 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }                                                                                                                                                                                      
                                                                                                                                                                                          
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7053&shareCode=KBBILKJKNAB&afp1=bd' });                                                                                                                          
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=7053&shareCode=B8GOCJSYQK4&afp1=bd' });                                                                                                                          
     res.end();                                                                                                                                                                           
   }
